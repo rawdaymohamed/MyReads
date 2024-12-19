119d0fc9d98a31416dff78593b7bd4f0e475f1ac
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-12-19 14:50:48 +0200

    deploy on netlify

---
+++ build/precache-manifest.6c0a50d16b616c2e0276a41e36f3783a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "581c16ef01569d059450",
+    "url": "/static/js/main.581c16ef.chunk.js"
+  },
+  {
+    "revision": "8e27acaa64713ce8c9f3",
+    "url": "/static/js/1.8e27acaa.chunk.js"
+  },
+  {
+    "revision": "581c16ef01569d059450",
+    "url": "/static/css/main.b092cb09.chunk.css"
+  },
+  {
+    "revision": "26f80b0b93ecd53f0af5a07ea4b64aae",
+    "url": "/index.html"
+  }
+];
